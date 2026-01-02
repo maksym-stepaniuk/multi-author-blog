b86500d54b02a04c514604a6dfe5fde4871049c3
Maksym Stepaniuk <maximstepanuk4356@gmail.com>
2026-01-02 19:24:43 +0100

    init

---
+++ src/main/java/pl/maxim/blog/MultiAuthorBlogApplication.java
@@ -0,0 +1,13 @@
+package pl.maxim.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAuthorBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiAuthorBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/maxim/blog/MultiAuthorBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maxim.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiAuthorBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
